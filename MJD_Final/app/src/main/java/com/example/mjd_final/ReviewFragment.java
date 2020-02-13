e4957c0c0ba8f2ee0d4923b0f0508e8369d7ca89
simpson9241 <simpson9241@gmail.com>
2020-02-14 07:16:04 +0900

    commit

--- MJD_Final/app/src/main/java/com/example/mjd_final/ReviewFragment.java
+++ MJD_Final/app/src/main/java/com/example/mjd_final/ReviewFragment.java
@@ -11,6 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+
+import java.util.Arrays;
+
 
 public class ReviewFragment extends Fragment {
 
@@ -18,6 +25,13 @@ public class ReviewFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
 
         View root = inflater.inflate(R.layout.activity_review_fragment,container,false);
+        Places.initialize(getActivity().getApplicationContext(),"AIzaSyCRuMEjdNWzmNU_Zu6RJcdjr-dnJ5OdqIU");
+        PlacesClient placesClient = Places.createClient(getActivity());
+
+        AutocompleteSupportFragment autocompleteSupportFragment =
+                (AutocompleteSupportFragment)getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment2);
+
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,Place.Field.NAME,Place.Field.LAT_LNG,Place.Field.ADDRESS_COMPONENTS));
 
         Spinner spinner = (Spinner) root.findViewById(R.id.spinner);
 
