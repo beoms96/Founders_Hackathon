925701291d41c0287f6e711c1b8c61e775fd4ad1
simpson9241 <simpson9241@gmail.com>
2020-02-14 01:28:36 +0900

    commit

--- MJD_Final/app/src/main/java/com/example/mjd_final/ReviewFragment.java
+++ MJD_Final/app/src/main/java/com/example/mjd_final/ReviewFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,18 @@ public class ReviewFragment extends Fragment {
 
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.activity_review_fragment,container,false);
+
+        View root = inflater.inflate(R.layout.activity_review_fragment,container,false);
+
+        Spinner spinner = (Spinner) root.findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(getActivity(),
+                R.array.sort_array,android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        return root;
     }
 }
